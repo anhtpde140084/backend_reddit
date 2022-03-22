c793be09806b820765d8bc50c5121853af2c1c25
tran phi anh <anhtpde140084@fpt.edu.vn>
2022-03-23 00:24:45 +0700

    first commit

---
+++ src/main/java/com/example/springredditclone/model/Post.java
@@ -0,0 +1,40 @@
+package com.example.springredditclone.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.lang.Nullable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.persistence.*;
+import java.time.Instant;
+
+import static javax.persistence.GenerationType.SEQUENCE;
+
+@Data
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Post {
+    @Id
+    @GeneratedValue(strategy = SEQUENCE)
+    private Long postId;
+    @NotBlank(message = "Post Name cannot be empty or Null")
+    private String postName;
+    @Nullable
+    @Lob
+    private String description;
+    private Integer voteCount;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId", referencedColumnName = "userId")
+    private User user;
+    private Instant createdDate;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id", referencedColumnName = "id")
+    private Subreddit subreddit;
+}
